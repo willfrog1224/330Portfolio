c95535b493eff054d5ce1c184c6039fdb7d4d068
Jillian Anderson <willfrog1224@yahoo.com>
2022-04-19 20:13:30 -0700

    u

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ const links = [
   ]
 
 function loadIndex() {
+    const ol = document.querySelector("#LinksList");
 
-  
+    links.forEach( link => {
+      const link = document.createElement("li");
+      const href = document.createElement("a");
+      href.setAttribute("href", link.url);
+      href.innerText = link.label;
+
+      link.appendChild(href);
+      ol.appendChild(link);
+    })
+
 }
