bba9cee6c864b164bef2dcb15741357829ce299f
Jillian Anderson <willfrog1224@yahoo.com>
2022-06-18 22:04:49 -0700

    update

--- js/main.js
+++ js/main.js
@@ -35,6 +35,10 @@ const links = [
       label: "Week9 notes",
       url: "week9/index.html"
     },
+    {
+      label: "Week10 notes",
+      url: "week10/index.html"
+    },
   ]
 
 function loadIndex() {
