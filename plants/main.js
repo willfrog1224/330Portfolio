5838819075931bdac7ed047d5e99aa3fdf5aad17
Jillian Anderson <willfrog1224@yahoo.com>
2022-07-18 19:02:52 -0700

    update

--- plants/main.js
+++ plants/main.js
@@ -1,41 +1,50 @@
-import other from './other.js';
+// import other from './other.js';
 
 
-document.querySelector("#add").onclick = addNewPlant;
+// document.querySelector("#add").onclick = addNewPlant;
 
-const input = document.querySelector("#pltype");
+// const input = document.querySelector("#pltype");
 
-input.addEventListener('keypress', e => {
-    if (e.keyCode == '13') addNewPlant();
-})
+// input.addEventListener('keypress', e => {
+//     if (e.keyCode == '13') addNewPlant();
+// })
 
-loadPlant();
+// loadPlant();
 
-function addNewPlant(e) {
-    const plant = { id: Date.now(), content: input.value, completed: false };
-    input.value = "";
-    const plantID = createPlant(plant);
-    other.savePlant(plant);
-    loadPlant();
+// function addNewPlant(e) {
+//     const plant = { id: Date.now(), content: input.value, completed: false };
+//     input.value = "";
+//     const plantID = createPlant(plant);
+//     other.savePlant(plant);
+//     loadPlant();
 
-}
+// }
+async function loadPlants() {
+    const response = await fetch('https://willfrog1224.github.io/330Portfolio/plants/plants.json');
+    const plants = await response.json();
+    console.log(plants); 
+    loadPlant(plants)
+    
+  }
+  
+loadPlants()
 function createPlant(plant) {
     const plantDiv = document.createElement('div');
     plantDiv.classList.add('plant');
 
     const completeBtn = document.createElement('button');
-    completeBtn.setAttribute('data-id', plant.id);
+   // completeBtn.setAttribute('data-id', plant.id);
     completeBtn.classList.add('complete-btn');
 
     const plantContent = document.createElement('div')
-    plantContent.innerText = plant.content;
+    plantContent.innerText = plant.name;
     plantContent.classList.add('plant-content');
 
     const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('data-id', plant.id);
+   // deleteBtn.setAttribute('data-id', plant.id);
     deleteBtn.classList.add('plant-delete-btn');
     deleteBtn.innerText = "Delete";
-    deleteBtn.onclick = deletePlant;
+  //  deleteBtn.onclick = deletePlant;
 
     plantDiv.appendChild(completeBtn);
     plantDiv.appendChild(plantContent);
@@ -44,16 +53,16 @@ function createPlant(plant) {
     return plantDiv;
 
 }
-function addPlant(plantDiv) {
-    document.querySelector('#plant').appendChild(plantDiv);
-}
-function loadPlant() {
-    document.querySelector('#plant').innerHTML = '';
-    const plants = other.getPlant();
+//function addPlant(plantDiv) {
+  //  document.querySelector('#plant').appendChild(plantDiv);
+//}
+function loadPlant(array) {
+    let final_plant = document.querySelector('#plant').innerHTML = '';
+   // const plants = other.getPlant();
     
-    plants.forEach(plant => {
+    array.forEach(plant => {
         const el = createPlant(plant)
-        addPlant(el);
+        final_plant.appendChild(el)
     })
 }
 function deletePlant(e) {
